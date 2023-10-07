869819f337a3038cb36724d0fc955a7c0f1db385
mohamed-a95 <mohamedabdihakima95@gmail.com>
2023-10-07 18:42:07 +0200

    frontend och backend klar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,19 @@
 import "./App.css";
 import io from "socket.io-client";
 import { useState } from "react";
+import Chat from "./Chats";
 
 const socket = io.connect("http://localhost:3001");
 
 function App() {
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
 
   const joinRoom = () => {
     if (username !== "" && room !== "") {
       socket.emit("join_room", room);
+      setShowChat(true);
     }
   };
 
